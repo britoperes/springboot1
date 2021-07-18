83bed62ff9ca1fedfe56730d43f7877213d8c8ad
meli-jobrito <joao.brito@mercadolivre.com>
2021-07-18 19:28:21 -0300

    Repository de Topicos implementado (metade da aula 3)'

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,23 +3,26 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+	@Autowired
+	private TopicoRepository topicoRepository;
 
 	@RequestMapping("/topicos")
 //	@ResponseBody
 	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Título", "Mensagem", new Curso("Nome do curso", "Categoria"));
-//		topico.setId(111L);
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));		
+//		Topico topico = new Topico("Título", "Mensagem", new Curso("Nome do curso", "Categoria"));
+//		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		return TopicoDto.converter(topicoRepository.findAll());
 	}
 		
 }
